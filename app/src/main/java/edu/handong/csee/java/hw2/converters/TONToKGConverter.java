eaeb9a761ffc5321df9e43d1be1d1e5deb641188
Somy-John <john621145@gmail.com>
2021-05-06 16:33:07 +0900

    hah

---
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/TONToKGConverter.java
@@ -0,0 +1,27 @@
+package edu.handong.csee.java.hw2.converters;
+/**
+ * TON To KG Converter
+ */
+public class TONToKGConverter implements Convertible{
+   private double value;
+   /**
+    * setter
+    * @param fromValue 벨류를 받아와서 프라이빗 멤버인 value를 설정
+    */
+   public void setFromValue(double fromValue){
+      value = fromValue;
+   }
+   /**
+    * getter
+    * @return 프라이빗 멤버인 value를 리턴
+    */
+   public double getConvertedValue(){
+      return value;
+   }
+   /**
+    * value를 단위환산해주는 메소드
+    */
+   public void convert(){
+      value = value*1000;
+   }
+}
